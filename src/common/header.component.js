9d0c312a73b564af8b566bbedc9e78f11e97b420
Yatindra Kumar Khandal <yatindra.kumar@tothenew.com>
2017-12-29 12:18:41 +0530

    Header Design Completed

--- src/common/header.component.js
+++ src/common/header.component.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 class Header extends Component {
 
     render(){
         return(
             <div className="header-container">
-
+                <div className="site-logo pull-left">
+                    <Link to="/">
+                        Specs Store
+                    </Link>
+                </div>
+                <div className="utility-icons pull-right">
+                    <em title="Login"><i className="fa fa-user-circle" aria-hidden="true"/></em>
+                    <em title="Track Order"><i className="fa fa-truck" aria-hidden="true" /></em>
+                    <em title="WishList"><i className="fa fa-heart" aria-hidden="true" /></em>
+                    <em title="Cart"><i className="fa fa-shopping-cart" aria-hidden="true" /></em>
+                </div>
             </div>
         )
     }
